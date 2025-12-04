16ed6fd0739041bb5b93b8d3d9d7de9f0f959e66
MrHeiz97 <stumpf.sebastian@hzl-network.de>
2025-12-05 00:01:14 +0100

    fix: Enhance null-check for template variables in DeployModal

--- frontend/src/apps/DockerManager/components/TemplateGallery.tsx
+++ frontend/src/apps/DockerManager/components/TemplateGallery.tsx
@@ -76,7 +76,7 @@ function DeployModal({ template, isOpen, onClose, onDeploy, deploying = false }:
         </div>
 
         {/* Variables */}
-        {Object.keys(template.variables).length > 0 && (
+        {template.variables && Object.keys(template.variables).length > 0 && (
           <div>
             <label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-3">
               Configuration Variables
